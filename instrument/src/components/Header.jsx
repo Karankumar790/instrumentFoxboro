5252c26de0ea36a0007c4ce57a06588656156dd3
MkumarMonu <148427409+MkumarMonu@users.noreply.github.com>
2025-01-30 03:11:41 +0530

    all changes

--- instrument/src/components/Header.jsx
+++ instrument/src/components/Header.jsx
@@ -32,8 +32,7 @@ function Header() {
     "https://www.beckhoff.com/media/pictures/tiles/products/mx-system/mx-system_webp_85.webp",
     "https://www.beckhoff.com/media/pictures/tiles/products/mx-system/mx-system_webp_85.webp",
     "https://www.beckhoff.com/media/pictures/tiles/products/ipc/ipc_webp_85.webp",
-      
-  ]
+  ];
   const top100Films = [];
   return (
     <>
@@ -97,43 +96,55 @@ function Header() {
       </Grid2>
 
       <Grid2 container display="flex" justifyContent="center" m={1}>
-        <Grid2 size={{ lg: 8 }} p={1} overflow='hidden' border={'1px solid black'}>
-          <Box display="flex" justifyContent="center" >
-            <img  src="https://www.beckhoff.com/media/pictures/stages/news/distributed-drive-technology-stage-lowres_webp_85.webp" />
+        <Grid2
+          size={{ lg: 8 }}
+          p={1}
+          overflow="hidden"
+          border={"1px solid black"}
+        >
+          <Box display="flex" justifyContent="center">
+            <img src="https://www.beckhoff.com/media/pictures/stages/news/distributed-drive-technology-stage-lowres_webp_85.webp" />
           </Box>
-        <Box>
-          <Typography variant="h4">Industries </Typography>
-        </Box>
-        <Grid2 container spacing={2}>
-          {/* <Typography></Typography> */}
-          {images.map((src, index) => (
-            <Grid2 size={{ lg: 3, md: 3, sm: 6, xs: 12 }} border={'1px solid black'} key={index}>
-              <Card>
-                <CardMedia
-                  component="img"
-                  style={{ height: "30vh", width: "40vh" }}
-                  image={src}
-                />
-              </Card>
-            </Grid2>
-          ))}
-        </Grid2>
+          <Box>
+            <Typography variant="h4">Industries </Typography>
+          </Box>
+          <Grid2 container spacing={2}>
+            {/* <Typography></Typography> */}
+            {images.map((src, index) => (
+              <Grid2
+                size={{ lg: 3, md: 3, sm: 6, xs: 12 }}
+                border={"1px solid black"}
+                key={index}
+              >
+                <Card>
+                  <CardMedia
+                    component="img"
+                    style={{ height: "30vh", width: "40vh" }}
+                    image={src}
+                  />
+                </Card>
+              </Grid2>
+            ))}
+          </Grid2>
           <Typography variant="h4"> Industrial software</Typography>
-        <Grid2 container spacing={2} mt={1}>
-          {indus.map((src, index) => (
-            <Grid2 size={{ lg: 3, md: 3, sm: 6, xs: 12 }} border={'1px solid black'} key={index}>
-              <Card>
-                <CardMedia
-                  component="img"
-                  style={{ height: "30vh", width: "40vh" }}
-                  image={src}
-                />
-              </Card>
-            </Grid2>
-          ))}
+          <Grid2 container spacing={2} mt={1}>
+            {indus.map((src, index) => (
+              <Grid2
+                size={{ lg: 3, md: 3, sm: 6, xs: 12 }}
+                border={"1px solid black"}
+                key={index}
+              >
+                <Card>
+                  <CardMedia
+                    component="img"
+                    style={{ height: "30vh", width: "40vh" }}
+                    image={src}
+                  />
+                </Card>
+              </Grid2>
+            ))}
+          </Grid2>
         </Grid2>
-        </Grid2>
-
       </Grid2>
       <Grid2></Grid2>
 
