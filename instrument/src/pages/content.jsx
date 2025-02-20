f3b21737126d86c44dd32c682a2cc0a6bcf50acf
karankumar790 <karan20-cs@sanskar.org>
2025-02-20 17:37:40 +0530

    created all content and footer also

--- instrument/src/pages/content.jsx
+++ instrument/src/pages/content.jsx
@@ -2,12 +2,13 @@ import {
   Box,
   Button,
   Card,
+  CardContent,
   CardMedia,
   Collapse,
   Grid2,
   Typography,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PageContainer from "../components/HOC/PageContainer";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
@@ -54,54 +55,56 @@ function content() {
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  // Function to handle next image
   const handleNext = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images_animation.length);
   };
 
-  const handlePrevious = () => {
-    setCurrentIndex(
-      (prevIndex) =>
-        (prevIndex - 1 + images_animation.length) % images_animation.length
-    );
-  };
+  // Auto-change banner every 3 seconds
+  useEffect(() => {
+    const interval = setInterval(handleNext, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, []);
+
   return (
     <>
       <PageContainer showheader="true">
         <Grid2 container display="flex" justifyContent="center" mt={1}>
           <Grid2 mr={2} display={"flex"} alignItems={"center"}>
-            <Button variant="inherit" onClick={handlePrevious}>
-              <ArrowBackIosIcon sx={{ fontSize: "50px" }} />
-            </Button>
+            {/* <Button variant="inherit" onClick={handlePrevious}> */}
+            {/* <ArrowBackIosIcon sx={{ fontSize: "50px" }} /> */}
+            {/* </Button> */}
           </Grid2>
-          <Grid2 size={{ lg: 7 }}>
-            <Box display="flex" justifyContent="center" height="53.3vh">
+          <Grid2 size={{ lg: 9 }}>
+            <Box display="flex" justifyContent="center" height="45vh">
               <img
                 src={images_animation[currentIndex]}
                 alt="Carousel"
-                style={{ maxHeight: "100%", maxWidth: "100%" }}
+                style={{ maxHeight: "100%", maxWidth: "100%", width: "100%" }}
               />
             </Box>
           </Grid2>
           <Grid2 ml={3} display={"flex"} alignItems={"center"}>
-            <Button variant="inherit" onClick={handleNext}>
-              <ArrowForwardIosIcon sx={{ fontSize: "50px" }} />
-            </Button>
+            {/* <Button variant="inherit" onClick={handleNext}> */}
+            {/* <ArrowForwardIosIcon sx={{ fontSize: "50px" }} /> */}
+            {/* </Button> */}
           </Grid2>
         </Grid2>
         <Grid2 container display="flex" justifyContent="center" m={1}>
           <Grid2
-            size={{ lg: 7 }}
+            size={{ lg: 9 }}
             p={0}
             overflow="hidden"
             // border={"1px solid black"}
           >
             <Box>
-              <Typography variant="h4" mt={3}>
-                Industrial Automation{" "}
+              <Typography variant="h5" mt={2} mb={2} fontWeight={"bold"}>
+                Industrial Automation
               </Typography>
             </Box>
 
-            <Grid2 container spacing={2}>
+            <Grid2 container spacing={3}>
               {images.map((src, index) => (
                 <Grid2
                   bgcolor={"yellow"}
@@ -115,7 +118,7 @@ function content() {
                         // sx={{objectFit:"cover",objectPosition:'center'}}
                         style={{
                           height: "30vh",
-                          width: "30vh",
+                          width: "40vh",
                           objectFit: "cover",
                           objectPosition: "left",
                           background:
@@ -178,11 +181,10 @@ function content() {
                 </Grid2>
               ))}
             </Grid2>
-            <Typography variant="h4" mt={3}>
-              {" "}
-              Industrial software
+            <Typography variant="h5" mt={3} mb={2} fontWeight={"bold"}>
+              Industrial Software
             </Typography>
-            <Grid2 container spacing={2} mt={1}>
+            <Grid2 container spacing={3} mt={1}>
               {indus.map((src, index) => (
                 <Grid2
                   size={{ lg: 3, md: 3, sm: 6, xs: 12 }}
@@ -194,7 +196,7 @@ function content() {
                       component="img"
                       style={{
                         height: "30vh",
-                        width: "30vh",
+                        width: "40vh",
                         objectFit: "cover",
                         background:
                           "linear-gradient(49deg, rgb(245, 244, 244), rgb(170, 170, 219) 100%) ",
@@ -253,11 +255,11 @@ function content() {
                 </Grid2>
               ))}
             </Grid2>
-
-            <Typography variant="h4" mt={3}>
-              Engineering Services
-            </Typography>
-            <Grid2 display="flex" m={1} flexDirection="row" gap={2}>
+            <Grid2 display={"flex"} mt={2} mb={2} gap={"50%"} >
+              <Typography variant="h5" fontWeight={"bold"}>Engineering Services</Typography>
+              <Typography variant="h5" fontWeight={"bold"}>New Product Line</Typography>
+            </Grid2>
+            <Grid2 display="flex" flexDirection="row" gap={2} mb={4}>
               <Grid2 size={{ lg: 8 }}>
                 {/* <img
                   src="https://www.beckhoff.com/media/pictures/cards/news/scheugenpflug-application-report-teaser_webp_85.webp"
@@ -278,11 +280,12 @@ function content() {
                     }}
                     alt="Layer Seven Application"
                   />
-                  <Box width={'10px'}  bgcolor={'yellow'}>
+                  <Box width={"10px"} bgcolor={"yellow"}>
                     <Typography
                       variant="h6"
                       sx={{
                         position: "absolute",
+                        right: 20,
                         bottom: 20,
                         left: 20,
                         color: "black",
@@ -291,6 +294,13 @@ function content() {
                         borderRadius: "5px",
                       }}
                     >
+                      <Typography
+                        variant="h6"
+                        color="black"
+                        fontWeight={"bold"}
+                      >
+                        PC-based control for all-electric blow molding machines
+                      </Typography>
                       In the extensive central warehouse of Australian office
                       equipment supplier Officeworks, Layer Seven Automation has
                       replaced the proprietary, outdated warehouse logistics
@@ -299,16 +309,53 @@ function content() {
                   </Box>
                 </Box>
               </Grid2>
-              <Grid2 size={{ lg: 4 }}>
-                <img
-                  src="https://www.beckhoff.com/media/pictures/cards/news/layer-seven-application-teaser_webp_85.webp"
-                  style={{
-                    objectFit: "cover",
+              {/* src="https://www.beckhoff.com/media/pictures/cards/news/layer-seven-application-teaser_webp_85.webp" */}
+              <Grid2 size={{ lg: 4 }} >
+                <Card
+                  sx={{
                     width: "100%",
                     height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
                   }}
-                  alt=""
-                />
+                >
+                  {/* Image */}
+                  <CardMedia
+                    component="img"
+                    image="https://www.beckhoff.com/media/pictures/cards/news/layer-seven-application-teaser_webp_85.webp"
+                    alt="Layer Seven Automation"
+                    sx={{
+                      width: "100%",
+                      height: "350px",
+                      objectFit: "cover",
+                    }}
+                  />
+
+                  {/* Content Section */}
+                  <CardContent
+                    sx={{
+                      flexGrow: 1,
+                      backgroundColor: "rgba(237, 231, 231, 0.7)",
+                    }}
+                  >
+                    <Typography
+                      variant="h6"
+                      color="black"
+                      // sx={{ fontWeight: "bold" }}
+                    >
+                      <Typography
+                        variant="h6"
+                        color="black"
+                        fontWeight={"bold"}
+                      >
+                        Flexible, automated production of valuable fertilizer from slurry
+                      </Typography>
+                      Layer Seven Automation has replaced the proprietary,
+                      outdated warehouse logistics control technology with
+                      PC-based control.
+                    </Typography>
+                  </CardContent>
+                </Card>
               </Grid2>
             </Grid2>
           </Grid2>
