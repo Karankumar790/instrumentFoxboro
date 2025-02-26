ff870250764f7f3fda2598370d077619067c7735
karankumar790 <karan20-cs@sanskar.org>
2025-02-26 15:26:00 +0530

    Normalize line endings

--- instrument/src/pages/service.jsx
+++ instrument/src/pages/service.jsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { Card, Grid2, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  Grid2,
+  styled,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Services from "../../public/assets/service.webp";
 import PageContainer from "../components/HOC/PageContainer";
 import Header from "../components/Header";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 function service() {
   const customer = [
@@ -23,15 +32,10 @@ function service() {
       Value: "",
     },
     {
-      Text: "Company Name",
-      Value: "",
-    },
-    {
-      Text: "Address",
+      Text: "Company ",
       Value: "",
     },
   ];
-
   const Details = [
     {
       Text: "City",
@@ -46,47 +50,63 @@ function service() {
       Value: "",
     },
     {
-      Text: "Postal Code",
+      Text: "ZIP Code",
       Value: "",
     },
-    {
-      Text: "Description",
-      Value: "",
-    },
+    // {
+    //   Text: "Problem",
+    //   Value: "",
+    // },
   ];
+  const VisuallyHiddenInput = styled("input")({
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
+    height: 1,
+    overflow: "hidden",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+  });
   return (
     <>
-      {/* <PageContainer showheader="true"> */}
-      <Header/>
+      <Header />
       <Grid2
         container
         sx={{
           height: "83.8vh",
-          backgroundImage: `url(${Services})`, 
-          backgroundSize: "cover", 
-          backgroundPosition: "center", 
+          backgroundImage: `url(${Services})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
-        bgcolor={"blue"}
       >
         <Grid2
           size={{ lg: 12 }}
+          mr={2}
           display={"flex"}
-          justifyContent={"center"}
+          // justifyContent={""}
           alignItems={"center"}
         >
           <Card
-            sx={{ borderRadius: "25px", height: "72vh" }}
+            sx={{
+              borderRadius: "25px",
+              height: "71vh",
+              ml: "55%",
+              width: "55%",
+            }}
             borderRadius={2}
             p={2}
           >
-            <Typography variant="h5" fontWeight={"bold"} m={1} mt={2}>
+            <Typography variant="h5" fontWeight={"bold"} ml={1.5} mt={2}>
+              Find Service Engineer Near You
+            </Typography>
+            {/* <Typography variant="h6" fontWeight={"bold"} ml={1.5}>
               Customer Support (7*24)
-            </Typography>
-            <Typography variant="h5" fontWeight={"bold"} m={1}>
+            </Typography> */}
+            {/* <Typography variant="h5" fontWeight={"bold"} mt={1} ml={1.5}>
               Customer Details
-            </Typography>
-            <Grid2 p={1}>
-              <Grid2 size={{ lg: 12 }}>
+            </Typography> */}
+            <Grid2 container p={1} display={"flex"} flexDirection={"row"}>
+              <Grid2 size={{ lg: 6 }}>
                 {customer.map((value, index) => (
                   <Typography
                     gap={8}
@@ -108,7 +128,8 @@ function service() {
                   </Typography>
                 ))}
               </Grid2>
-              <Grid2 size={{ lg: 12 }}>
+
+              <Grid2 size={{ lg: 5 }} ml={8}>
                 {Details.map((value, index) => (
                   <Typography
                     variant="h6"
@@ -128,13 +149,106 @@ function service() {
                   </Typography>
                 ))}
               </Grid2>
+              <Grid2 ml={1}>
+                <Typography  variant="h6">Problem</Typography>
+                <Box sx={{ width: 815, maxWidth: "100%", height:"150px" }}>
+                  <TextField fullWidth   sx={{ height: "150px" }}
+      multiline
+      rows={5}  />
+                </Box>
+              </Grid2>
+              <Grid2 size={{ lg: 12 }} display={"flex"}>
+                <Box m={1}>
+                  <Button
+                    sx={{ width: "100%" }}
+                    component="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    startIcon={<CloudUploadIcon />}
+                  >
+                    Front Upload files
+                    <VisuallyHiddenInput
+                      type="file"
+                      onChange={(event) => console.log(event.target.files)}
+                      multiple
+                    />
+                  </Button>
+                  <Button
+                    sx={{ width: "100%", mt: "2%" }}
+                    component="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    startIcon={<CloudUploadIcon />}
+                  >
+                    Back Upload files
+                    <VisuallyHiddenInput
+                      type="file"
+                      onChange={(event) => console.log(event.target.files)}
+                      multiple
+                    />
+                  </Button>
+                </Box>
+                <Box m={1}>
+                  <Button
+                    sx={{ width: "100%" }}
+                    component="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    startIcon={<CloudUploadIcon />}
+                  >
+                    Left Upload files
+                    <VisuallyHiddenInput
+                      type="file"
+                      onChange={(event) => console.log(event.target.files)}
+                      multiple
+                    />
+                  </Button>
+                  <Button
+                    sx={{ width: "100%", mt: "2%" }}
+                    component="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    startIcon={<CloudUploadIcon />}
+                  >
+                    Right Upload files
+                    <VisuallyHiddenInput
+                      type="file"
+                      onChange={(event) => console.log(event.target.files)}
+                      multiple
+                    />
+                  </Button>
+                </Box>
+              </Grid2>
+              <Box m={1} width={"100%"}  display={"flex"} justifyContent={"end"}>
+                <Button
+                  sx={{ width: "15%", bgcolor: "orange" }}
+                  variant="contained"
+                >
+                  Submit
+                </Button>
+              </Box>
             </Grid2>
           </Card>
         </Grid2>
+        {/* <Grid2 size={{ lg: 2.7 }} mt={5}>
+          <Typography variant="h5" fontWeight={"bold"}>
+            Find Service Engineer Near by You
+          </Typography>
+          <Typography
+            variant="h6"
+            fontWeight={"bold"}
+            display={"flex"}
+            justifyContent={"center"}
+          >
+            Customer Support (7*24)
+          </Typography>
+        </Grid2> */}
       </Grid2>
-      {/* </PageContainer> */}
     </>
   );
 }
-
 export default service;
